f721852077e22484149e273599661cb2d9bada25
jki503 <jki503@naver.com>
2022-05-08 16:52:07 +0900

    chore : 스키마 product, order, order_products 추가

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -3,7 +3,47 @@ CREATE TABLE customers
     customer_id BINARY(16) PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    create_at DATETIME(6) NOT NULL,
+    created_at DATETIME(6) NOT NULL,
     last_login_at DATETIME(6) NOT NULL,
     CONSTRAINT unique_customer_email UNIQUE (email)
+);
+
+CREATE TABLE products
+(
+    product_id   BINARY(16) PRIMARY KEY,
+    product_name VARCHAR(20) NOT NULL,
+    category     VARCHAR(50) NOT NULL,
+    price        bigint      NOT NULL,
+    description  VARCHAR(500) DEFAULT NULL,
+    created_at   datetime(6) NOT NULL,
+    updated_at   datetime(6)  DEFAULT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id     binary(16) PRIMARY KEY,
+    customer_id binary(16) NOT NULL,
+    email        VARCHAR(50)  NOT NULL,
+    address      VARCHAR(200) NOT NULL,
+    postcode     VARCHAR(200) NOT NULL,
+    order_status VARCHAR(50)  NOT NULL,
+    created_at   datetime(6)  NOT NULL,
+    updated_at   datetime(6) DEFAULT NULL,
+    INDEX(customer_id),
+    CONSTRAINT fk_orders_to_customer FOREIGN KEY(customer_id) REFERENCES customers (customer_id) ON DELETE CASCADE
+);
+
+CREATE TABLE order_products
+(
+    seq        bigint      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    order_id   binary(16)  NOT NULL,
+    product_id binary(16)  NOT NULL,
+    category   VARCHAR(50) NOT NULL,
+    price      bigint      NOT NULL,
+    quantity   int         NOT NULL,
+    created_at datetime(6) NOT NULL,
+    updated_at datetime(6) DEFAULT NULL,
+    INDEX (order_id),
+    CONSTRAINT fk_order_items_to_order FOREIGN KEY (order_id) REFERENCES orders (order_id) ON DELETE CASCADE,
+    CONSTRAINT fk_order_items_to_product FOREIGN KEY (product_id) REFERENCES products (product_id)
 );
